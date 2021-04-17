92d77449b48e1209325d1a5f05932dcb8cbb7197
Magdalena Drożdż <magdalena.drozdz@mnm.bio>
2021-04-17 12:11:23 +0200

    folder reorg (last one i promise)

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,6 +1,6 @@
-Rscript 01_DNA.R ./input/rosalind_dna.txt
+Rscript ./scripts/01_DNA.R ./input/rosalind_dna.txt
 
-Rscript 02_RNA.R ./input/rosalind_rna.txt
+Rscript ./scripts/02_RNA.R ./input/rosalind_rna.txt
 
-Rscript 03_REVC.R ./input/rosalind_revc.txt
+Rscript ./scripts/03_REVC.R ./input/rosalind_revc.txt
 
