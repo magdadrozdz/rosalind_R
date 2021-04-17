47f279766cbea2f4c95a575a0077b785e5c89dfa
Magdalena Drożdż <magdalena.drozdz@mnm.bio>
2021-04-17 16:57:29 +0200

    added 05_GC

--- run_scripts.sh
+++ run_scripts.sh
@@ -5,3 +5,5 @@ Rscript ./scripts/02_RNA.R ./input/rosalind_rna.txt
 Rscript ./scripts/03_REVC.R ./input/rosalind_revc.txt
 
 Rscript ./scripts/04_IPRB.R ./input/rosalind_iprb.txt
+
+Rscript ./scripts/05_GC.R ./input/rosalind_gc.txt
---
+++ scripts/05_GC.R
@@ -0,0 +1,23 @@
+suppressMessages(library(tidyverse))
+suppressMessages(library(here))
+suppressMessages(library(Biostrings))
+args = commandArgs(trailingOnly=TRUE)
+
+dna_string <- readDNAStringSet(args[1])
+
+#dna_string <- readDNAStringSet(here('input', 'rosalind_gc.txt'))
+
+top_count <- alphabetFrequency(dna_string, baseOnly=T, collapse=F) %>% 
+  data.frame() %>% 
+  rowwise() %>%
+  mutate(GC_content = sprintf("%0.6f", sum(C,G)/sum(A, C, `T`, G)*100)) %>%
+  ungroup() %>%
+  rowid_to_column(var = 'row_id') %>%
+  top_n(1) %>% 
+  select(row_id, GC_content)
+
+result <- paste0(names(dna_string[top_count$row_id,]), '\n', top_count$GC_content)
+
+cat(result)
+
+write_file(result, here('output', '05_GC.txt'))
