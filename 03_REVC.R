6c52c4861d3b4dc8a6cf1c2a5d1a45d611f35b3e
Magdalena Drożdż <magdalena.drozdz@mnm.bio>
2021-04-17 12:04:15 +0200

    Added #3 reverse complement

---
+++ 03_REVC.R
@@ -0,0 +1,22 @@
+suppressMessages(library(readr))
+suppressMessages(library(magrittr))
+suppressMessages(library(Biostrings))
+args = commandArgs(trailingOnly=TRUE)
+
+dna_string <- read_file(args[1])
+
+dna_string <- read_file('./data/rosalind_revc.txt')
+
+# sample_dataset 
+# dna_string <- 'AAAACCCGGT'
+
+rev_complement <- dna_string %>%
+  trimws() %>%
+  DNAString() %>% 
+  reverseComplement() %>%
+  as.character() %>%
+  paste0(collapse = ' ')
+  
+print(rev_complement)
+
+write_file(rev_complement, './output/03_REVC.txt')
