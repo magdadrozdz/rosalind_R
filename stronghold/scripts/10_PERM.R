86dd53ddc01ec17836571f05cdb4b7d3ad96aabf
Magdalena Drożdż <magdalena.drozdz@ludwig.ox.ac.uk>
2021-06-19 11:34:16 +0200

    simplified 10_PERM

--- stronghold/scripts/10_PERM.R
+++ stronghold/scripts/10_PERM.R
@@ -4,15 +4,10 @@ library(tidyverse)
 
 args = commandArgs(trailingOnly=TRUE) 
 n = scan(args[1])
+output_file <- here('stronghold', 'output', '10_PERM.txt')
 
 perms <- data.frame(permutations(n, n))
 
-write.table(perms, file = here('stronghold', 'output', '10_PERM_table.txt'), col.names = F, row.names = F)
-
-result <- read_file(here('stronghold', 'output', '10_PERM_table.txt'))
-
-result <- paste0(nrow(perms), '\n', result)
-
-sink(here('stronghold', 'output', '10_PERM.txt'))
-cat(result)
-sink()
+# save result
+cat(paste0(nrow(perms), '\n'), file = outuput_file)
+write.table(perms, file = outuput_file, sep = ' ', col.names = F, row.names = F, append = T)
